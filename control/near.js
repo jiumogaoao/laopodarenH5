e019f9fe839861050011168717a28f589284c913
jiumogaoao <394127821@qq.com>
2016-03-04 18:04:16 +0800

    change
    change

---
+++ control/near.js
@@ -0,0 +1,39 @@
+app.control.set({
+	name:"near",
+	par:[],
+	fn:function(data){
+		function viewDone(){
+			//var myScroll = new IScroll('#zoneMain', {  });
+			var offset=0;
+			$(".near_page #nearMain").unbind("swipeleft").bind("swipeleft",function(){
+				if(offset<$(".near_page .subPage").length-1){
+					offset++;
+					$(".nav_module .nav_point").removeClass('hl');
+					$(".nav_module .nav_point").eq(offset).addClass("hl");
+					$(".nav_module .heightLine").css({"transition-timing-function": "cubic-bezier(0.1, 0.57, 0.1, 1)", "transition-duration": "1000ms","transform":"translate("+offset+"00%,0px ) translateZ(0px)"});
+					$(".near_page #nearRoll").css({"transition-timing-function": "cubic-bezier(0.1, 0.57, 0.1, 1)", "transition-duration": "1000ms","transform":"translate(-"+(offset*750)+"px,0px ) translateZ(0px)"});
+				}
+			});
+			$(".near_page #nearMain").unbind("swiperight").bind("swiperight",function(){
+				if(offset>0){
+					offset--;
+					$(".nav_module .nav_point").removeClass('hl');
+					$(".nav_module .nav_point").eq(offset).addClass("hl");
+					$(".nav_module .heightLine").css({"transition-timing-function": "cubic-bezier(0.1, 0.57, 0.1, 1)", "transition-duration": "1000ms","transform":"translate("+offset+"00%,0px ) translateZ(0px)"});
+					$(".near_page #nearRoll").css({"transition-timing-function": "cubic-bezier(0.1, 0.57, 0.1, 1)", "transition-duration": "1000ms","transform":"translate(-"+(offset*750)+"px,0px ) translateZ(0px)"});
+				}
+			});
+		}
+		function headDone(){
+			 $(".head_module .left").unbind("tap").bind("tap",function(){
+				app.control.back();
+			});
+		}
+		function footDone(){
+
+		}
+		app.view.head.show("buttonTitleButton_head",{title:"附近",right:"筛选"},headDone);
+		app.view.foot.hide(footDone);
+		app.view.main.sugest("near_page",data,data.state,"size",viewDone);
+	}
+});
