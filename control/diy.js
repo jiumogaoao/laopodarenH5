5d7b9d4777fdec5aceecd8fc1736e8a7e2c2f7dd
jiumogaoao <394127821@qq.com>
2016-03-07 17:38:21 +0800

    change
    change

---
+++ control/diy.js
@@ -0,0 +1,21 @@
+app.control.set({
+	name:"diy",
+	par:[],
+	fn:function(data){
+		function viewDone(){
+			var myScroll = new IScroll('#diyMain', {  });
+			
+		}
+		function headDone(){
+			$(".head_module .left").unbind("tap").bind("tap",function(){
+				app.control.back();
+			});
+		}
+		function footDone(){
+
+		}
+		app.view.head.show("buttonTitleButton_head",{"title":"个性化中心","right":"装扮开关"},headDone);
+		app.view.foot.hide(footDone);
+		app.view.main.sugest("diy_page",data,data.state,"size",viewDone);
+	}
+});
